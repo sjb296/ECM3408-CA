3f9ab791597a7057f86eb28ee7a3b2370c13ff64
Samuel Juul Barker <sjb296@exeter.ac.uk>
2024-02-28 15:27:27 +0000

    Firebase basic ops

--- sc/db/firebase_helper.py
+++ sc/db/firebase_helper.py
@@ -1,26 +1,65 @@
+import requests
+import os
+from flask import request
+
+URL = os.environ["FBASE"]
+params = '?orderBy="$key"&startAt="B1"&limitToFirst=1'
+
+
 def list_cells():
     """Return a JSON list containing the coordinates of all cells#
     that have data in them"
 
     e.g. `["B1, "C6", ...]`
     """
-    pass
+    res = requests.get(URL + ".json")
+    if res.ok:
+        return res.json()
+    else:
+        return "", 500
 
 
-def get_cell():
+def get_cell(id):
     """Given the label of a cell, return a JSON object containing
     the label and the data stored in the cell
 
     e.g. `{"B1", "6"}`"""
-    pass
+    params = f'?orderBy="$key"&startAt="{id}"&limitToFirst=1'
+    res = requests.get(URL + ".json" + params)
+    print(res.json())
+    if res.json() == None:
+        return "", 404
+    elif res.ok:
+        return res.json()
+    else:
+        return "", 500
 
 
-def create_cell():
+def create_cell(id):
     """Create a cell with the given label and data. If it
-    already exists, update it(?)"""
-    pass
+    already exists, update it"""
+    req_json = request.get_json()
+
+    res = requests.patch(
+        URL + ".json", f'{{ "{req_json["id"]}": "{req_json["formula"]}" }}'
+    )
+    if res.ok:
+        return res.json()
+    else:
+        print(res.json())
+        return "", 500
 
 
-def delete_cell():
+def delete_cell(id):
     """Delete a cell with a given label."""
-    pass
+    # Check it exists
+    code = list(get_cell(id).values())[0]
+    if not code:
+        return "", 404
+
+    res = requests.delete(URL + f"{id}.json")
+
+    if res.json() == None:  # deletion returns null
+        return "", 200
+    else:
+        return "", 500
